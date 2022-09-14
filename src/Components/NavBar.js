8b07818130bd8f993e3811ab25ac3c9640b7c77c
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-09-15 00:58:08 +0300

    update the navbar component

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+
 
 const NavBar = () => {
   return (
     <div>
-      
+          <NavLink exact to="/" className="nav-button"> My Events </NavLink>
+          <NavLink exact to="/" className="nav-button"> New Event </NavLink>
+          <NavLink exact to="/" className="nav-button"> New Venue </NavLink>
+          <NavLink to='/' className="nav-button">Artist Stats</NavLink>
+
     </div>
   )
 }
