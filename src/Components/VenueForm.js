c6d6bcaa98e2a2e473e4246ff22cb994240464b0
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-09-15 00:03:57 +0300

    create functions for each item

--- src/Components/VenueForm.js
+++ src/Components/VenueForm.js
@@ -10,7 +10,43 @@ const VenueForm = () => {
     const [capacity, setCapacity] = useState('');
     let navigate = useNavigate();
 
+function handleVenueChange(e) {
+    setVenue(e.target.value)
+}
+        
+function handleAddressChange(e) {
+    setAddress(e.target.value)
+ }
+        
+function handlePhoneChange(e) {
+    setPhone(e.target.value)
+}
+            
+function handleCapacityChange(e) {
+    setCapacity(e.target.value)
+ }
+        
+function handleSubmit(e) {
+    e.preventDefault();
+        
+    const newVenueObj = {
+        venue_name: venue,
+        address: address,
+        phone: phone,
+        capacity: capacity
+        }
 
+        
+        fetch("http://localhost:9292/add-venue",{
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newVenueObj)
+          })
+          .then(response => response.json())
+          .then((data) => console.log(data))  
+          .then(navigate("/create-event", { replace: true }))
+       
+      } 
 
 
   return (
