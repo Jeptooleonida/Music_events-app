80cfba950b3fa24be59d96aba71313b0dc30088a
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-09-15 00:57:17 +0300

    update the event list with functions

--- src/Components/EventList.js
+++ src/Components/EventList.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import EventCard from './EventCard';
+import Search from './Search';
 
-const EventList = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+
+const EventList = ({data, handleDeleteEvent, handleUpdateEvent}) => {
+    const [dataIndex, setDataIndex] = useState(0)
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const eventList = [...data]
+    .filter((data) => { 
+      return data.user.username.toLowerCase().includes(searchTerm.toLowerCase());
+    })
+      .slice(dataIndex, dataIndex + 8)
+  
+      .map((data) => 
+      <EventCard
+          key={data.id}
+          data={data}
+          handleDeleteEvent={handleDeleteEvent}
+          handleUpdateEvent={handleUpdateEvent}
+      />)
+  
+      function handleClickMore() {
+        setDataIndex((dataIndex) => (dataIndex + 8) % data.length);
+      }
+  
+    return (
+      <>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <div className="event-list">
+      {eventList}
+      </div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <div className="next-container">
+          <button className="next-button" onClick={handleClickMore}>Next</button>
+      </div>
+      </>    
+    )
+  }
+  
 
 export default EventList
