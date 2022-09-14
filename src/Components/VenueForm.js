ed3c6961bbd4fb5d5c1417839aaf81b0d04edf72
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-09-14 23:58:43 +0300

    declare states

--- src/Components/VenueForm.js
+++ src/Components/VenueForm.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 
 const VenueForm = () => {
+
+    const [venue, setVenue] = useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState("");
+    const [capacity, setCapacity] = useState('');
+    let navigate = useNavigate();
+
+
+
+
   return (
     <div>
       
